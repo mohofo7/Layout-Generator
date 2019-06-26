3a4f4e1b611bc85a62fbe509f29e7fb06249004a
MohammadHosein Forghani <mohofo7@gmail.com>
2019-06-26 10:07:49 +0430

    container parse string and generate slots

--- src/components/container/container.js
+++ src/components/container/container.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import Slot from './../slot/slot'
-
+import parseString from './../../utils/parseString'
 class container extends React.Component{
 
     render(){
+        let parsedString = parseString(this.props.layoutString)
+        console.log(parsedString)
+        let slots = parsedString.map((slot)=>
+            <Slot className = {slot}/>
+        )
         return (
-            <div>
-                {this.props.layoutString}
-                <Slot />
-                <Slot />
-            </div>
+            <div>{slots}</div>
         )
     }
 }
--- src/components/slot/slot.js
+++ src/components/slot/slot.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Slot(props){
     return (
         <div>
-            slot
+            Slot
         </div>
     )
 }
